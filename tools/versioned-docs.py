d54a4397adc21fd37a699b117b01c049c1679769
eduardodoria <doriaeduardo@gmail.com>
2020-07-16 10:39:37 -0300

    Updates in build script

--- tools/versioned-docs.py
+++ tools/versioned-docs.py
@@ -2,7 +2,7 @@ import os
 
 import click
 import subprocess
-from git import Git
+import git
 
 overrides_main = '{% extends "base.html" %}\n'
 
@@ -12,29 +12,50 @@ old_version_block = (
     '{% endblock %}\n'
     )
 
+def getRepo(repo, dst, tag):
+    if not os.path.exists(dst):
+        print("Cloning %s repository and checkout to: %s" % (dst, tag), flush=True)
+        repo = git.Repo.clone_from(repo, dst)
+    else:
+        print("Checking out %s repository to: %s" % (dst, tag), flush=True)
+        repo = git.Repo(dst)
+
+    repo.git.checkout(tag)
+    repo.remotes['origin'].pull()
+    repo.submodule_update(recursive=False)
+
+    return repo
+
 @click.command()
-@click.option('--site-dir', type=click.Path(), default='site')
+@click.option('--site-dir', type=click.Path(), default='../site')
 @click.option('--tags', '-t', multiple=True)
 @click.option('--default', '-d', default='master')
 @click.option('--latest', '-l', default='master')
 def build_command(site_dir, tags, default, latest):
-    """Build the MkDocs documentation"""
-    
-    g = Git()
-    tags = tags or g.tag().splitlines()
 
-    g.checkout(default)
+    directory = "build"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    os.chdir(directory)
+
+    repo = getRepo('https://github.com/eduardodoria/supernova-docs.git', 'supernova-docs', 'master')
+
+    os.chdir('supernova-docs')
+    
+    tags = tags or repo.git.tag().splitlines()
+
+    repo.git.checkout(default)
     print("Building %s to %s" % (default, site_dir), flush=True)
     subprocess.run(['mkdocs', 'build', '--site-dir', site_dir, "--clean"]).check_returncode()
 
-    g.checkout(latest)
+    repo.git.checkout(latest)
     latest_dir = os.path.join(site_dir, 'latest')
     print("Building %s to %s" % (latest, latest_dir), flush=True)
     subprocess.run(['mkdocs', 'build', '--site-dir', latest_dir, "--clean"]).check_returncode()
 
     for tag in sorted(tags):
 
-        g.checkout(tag)
+        repo.git.checkout(tag)
 
         if not os.path.exists("mkdocs.yml"):
             print("Unable to build %s, as no mkdocs.yml was found" % (tag), flush=True)
@@ -58,9 +79,7 @@ def build_command(site_dir, tags, default, latest):
         print("Building %s to %s" % (tag, tag_dir), flush=True)
         subprocess.run(['mkdocs', 'build', '--site-dir', tag_dir, "--clean"]).check_returncode()
 
-        g.checkout(mainhtmlpath)
-
-    g.checkout('master')
+        repo.git.checkout(mainhtmlpath)
 
 if __name__ == '__main__':
     build_command()
